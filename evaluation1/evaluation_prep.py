0845deba09008a604b02f68fd7c5bf9e1c88e5c4
mathieu li tim cheong <mathieultc@mathieus-MacBook-Air.local>
2019-10-03 23:41:06 -0400

    .

--- evaluation1/evaluation_prep.py
+++ evaluation1/evaluation_prep.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 
 # exmaple 1
 def add(num1:int ,num2:int) -> int:
@@ -23,4 +23,25 @@ def sum_list(numbers: List[float]) -> float:
     """
 
     return sum(numbers)
+
+
+#example 3
+def word_count(words: List) -> Dict:
+    """ A function that takes a list of words and returns a dictionary where the key
+    is the word and the value is the number of times the word appeaers
+    Args:
+        param1: A list of words
+    Returns:
+        A dictionary where the key is the word and the value is the number of times it appears in the list
+    """
+    
+    word_dict = dict()
+    
+    for word in words:
+        count = words.count(word)
+        word_dict[word] = count
+
+    return word_dict
+
+
 
