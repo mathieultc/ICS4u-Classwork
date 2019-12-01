b4e01607104e92b8d8582ed1280170a90069d020
mathieu li tim cheong <mathieultc@mathieus-MacBook-Air.local>
2019-11-30 23:31:56 -0500

    algorithm

---
+++ algorithm/algorithm.py
@@ -0,0 +1,108 @@
+"""
+- Recursion: having a function call it itself
+- Base case: What is the final end-point of this algorithm?
+- Recursive case: Call the function again. (algorithm not complete)
+"""
+
+
+def count_down(n):
+    if n == 0: #base case
+        return
+
+    print(n)
+
+    count_down(n-1)# recursive case
+
+
+def sum_up_to(n: int) -> int:
+    if n == 1: #base case
+        return 1
+
+    return n + sum_up_to(n-1) #recursive case 
+  
+
+def factorial(n: int) -> int:
+    if n == 1:
+        return 1 #base case
+    elif n == 0:
+        return 1 #base case
+
+    return n * factorial(n - 1) #recursive case
+
+
+def fibonacci(n: int) -> int:
+    if n == 0:
+        return 0 #base case
+    if n == 1:
+        return 1 #base case
+
+    return fibonacci(n-1) + fibonacci(n-2) #recursive case
+
+
+def bunnyEars(n: int) -> int:
+    if n == 1:
+        return 2 #base case
+    elif n == 0:
+        return 0
+
+    return 2 + bunnyEars(n-1) #recursive case
+
+
+def bunnyEars2(n: int) -> int:
+    if n == 0: #base case
+        return 0
+    elif n % 2 == 0:
+        return 3 + bunnyEars2(n-1) #recursive case
+    else:
+        return 2 + bunnyEars2(n-1)
+
+
+def triangle(n: int) -> int:
+    if n == 0: 
+        return 0 #base case
+    elif n == 1: 
+        return 1
+
+    return n + triangle(n-1) #recursive case
+
+
+def sumDigits(n: int) -> int:
+    if n < 10: #base case
+        return n
+
+    return n % 10 + sumDigits(n//10) #recursive case
+
+
+def count7(n: int) -> int:
+    if n == 7:
+        return  1
+    elif str(7) not in str(n):
+        return 0
+    elif n % 10 == 7:
+        return 1 + count7(n//10)
+    else:
+        return count7(n//10)
+
+
+def count8(n: int) -> int:
+    if n == 8:
+        return 1
+
+    elif str(8) not in str(n):
+        return 0
+
+    elif n % 100 == 88:
+        return 3 + count8(n//100)
+
+    elif n % 10 == 8:
+        return 1 + count8(n//10)
+    
+
+    else:
+        return count8(n//10)
+
+ 
+
+
+
+   
---
+++ algorithm/test_algorithm.py
@@ -0,0 +1,270 @@
+# Recursion-1 > factorial (p154669)
+
+from algorithm import factorial, fibonacci, bunnyEars, bunnyEars2, sumDigits, triangle, count7, count8 
+
+#factorial
+def test_0():
+    assert factorial(1) == 1
+
+def test_1():
+    assert factorial(2) == 2
+
+def test_2():
+    assert factorial(3) == 6
+
+def test_3():
+    assert factorial(4) == 24
+
+def test_4():
+    assert factorial(5) == 120
+
+def test_5():
+    assert factorial(6) == 720
+
+def test_6():
+    assert factorial(7) == 5040
+
+def test_7():
+    assert factorial(8) == 40320
+
+def test_8():
+    assert factorial(12) == 479001600
+
+#fibonacci
+# Recursion-1 > fibonacci (p120015)
+
+
+def test_0():
+    assert fibonacci(0) == 0
+
+def test_1():
+    assert fibonacci(1) == 1
+
+def test_2():
+    assert fibonacci(2) == 1
+
+def test_3():
+    assert fibonacci(3) == 2
+
+def test_4():
+    assert fibonacci(4) == 3
+
+def test_5():
+    assert fibonacci(5) == 5
+
+def test_6():
+    assert fibonacci(6) == 8
+
+def test_7():
+    assert fibonacci(7) == 13
+
+
+#bunny ears 
+def test_0():
+    assert bunnyEars(0) == 0
+
+def test_1():
+    assert bunnyEars(1) == 2
+
+def test_2():
+    assert bunnyEars(2) == 4
+
+def test_3():
+    assert bunnyEars(3) == 6
+
+def test_4():
+    assert bunnyEars(4) == 8
+
+def test_5():
+    assert bunnyEars(5) == 10
+
+def test_6():
+    assert bunnyEars(12) == 24
+
+def test_7():
+    assert bunnyEars(50) == 100
+
+def test_8():
+    assert bunnyEars(234) == 468
+
+#bunny ears 2
+
+def test_0():
+    assert bunnyEars2(0) == 0
+
+def test_1():
+    assert bunnyEars2(1) == 2
+
+def test_2():
+    assert bunnyEars2(2) == 5
+
+def test_3():
+    assert bunnyEars2(3) == 7
+
+def test_4():
+    assert bunnyEars2(4) == 10
+
+def test_5():
+    assert bunnyEars2(5) == 12
+
+def test_6():
+    assert bunnyEars2(6) == 15
+
+def test_7():
+    assert bunnyEars2(10) == 25
+
+
+#sumDigits
+def test_0():
+    assert sumDigits(126) == 9
+
+def test_1():
+    assert sumDigits(49) == 13
+
+def test_2():
+    assert sumDigits(12) == 3
+
+def test_3():
+    assert sumDigits(10) == 1
+
+def test_4():
+    assert sumDigits(1) == 1
+
+def test_5():
+    assert sumDigits(0) == 0
+
+def test_6():
+    assert sumDigits(730) == 10
+
+def test_7():
+    assert sumDigits(1111) == 4
+
+def test_8():
+    assert sumDigits(11111) == 5
+
+def test_9():
+    assert sumDigits(10110) == 3
+
+def test_10():
+    assert sumDigits(235) == 10
+
+
+#triangle
+
+def test_0():
+    assert triangle(0) == 0
+
+def test_1():
+    assert triangle(1) == 1
+
+def test_2():
+    assert triangle(2) == 3
+
+def test_3():
+    assert triangle(3) == 6
+
+def test_4():
+    assert triangle(4) == 10
+
+def test_5():
+    assert triangle(5) == 15
+
+def test_6():
+    assert triangle(6) == 21
+
+def test_7():
+    assert triangle(7) == 28
+
+
+#count7
+def test_0():
+    assert count7(717) == 2
+
+def test_1():
+    assert count7(7) == 1
+
+def test_2():
+    assert count7(123) == 0
+
+def test_3():
+    assert count7(77) == 2
+
+def test_4():
+    assert count7(7123) == 1
+
+def test_5():
+    assert count7(771237) == 3
+
+def test_6():
+    assert count7(771737) == 4
+
+def test_7():
+    assert count7(47571) == 2
+
+def test_8():
+    assert count7(777777) == 6
+
+def test_9():
+    assert count7(70701277) == 4
+
+def test_10():
+    assert count7(777576197) == 5
+
+def test_11():
+    assert count7(99999) == 0
+
+def test_12():
+    assert count7(99799) == 1
+
+
+#count8
+def test_0():
+    assert count8(8) == 1
+
+def test_1():
+    assert count8(818) == 2
+
+def test_2():
+    assert count8(8818) == 4
+
+def test_3():
+    assert count8(8088) == 4
+
+def test_4():
+    assert count8(123) == 0
+
+def test_5():
+    assert count8(81238) == 2
+
+def test_6():
+    assert count8(88788) == 6
+
+def test_7():
+    assert count8(8234) == 1
+
+def test_8():
+    assert count8(2348) == 1
+
+def test_9():
+    assert count8(23884) == 3
+
+def test_10():
+    assert count8(0) == 0
+
+def test_11():
+    assert count8(1818188) == 5
+
+def test_12():
+    assert count8(8818181) == 5
+
+def test_13():
+    assert count8(1080) == 1
+
+def test_14():
+    assert count8(188) == 3
+
+def test_15():
+    assert count8(9898) == 2
+
+def test_16():
+    assert count8(78) == 1
