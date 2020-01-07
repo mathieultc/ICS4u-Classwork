51d492eb57096aaf8104e78e98ece7ae59859aa9
mathieu li tim cheong <mathieultc@mathieus-MacBook-Air.local>
2020-01-06 23:12:38 -0500

    format

--- rough 2/main.py
+++ rough 2/main.py
@@ -34,42 +34,42 @@ class MyGame(arcade.Window):
 
 
         self.obstacles_list = []
-        self.random_x = random.sample(range(25, WIDTH, 50), 8)
-        self.random_y = bubbleSort(random.sample(range(50, HEIGHT + 500 , 50), 15))
+        self.random_x = random.sample(range(25, WIDTH, 75),3)
+        self.random_y = bubbleSort(random.sample(range(25, HEIGHT), 9))
 
         self.sprite1 = arcade.Sprite(center_x=self.random_x[0], center_y=self.random_y[0] + 200)
-        self.sprite1.texture = arcade.load_texture(file_name="images/officeguy.png", scale = 0.05)
-
+        self.sprite1.texture = arcade.load_texture(file_name="images/officeguy.png", scale = 0.03)
         
         for (i, j) in zip(self.random_x, self.random_y):
             self.obstacle = arcade.Sprite(center_x=i, center_y=j)
-            self.obstacle.texture = arcade.load_texture(file_name="images/platform.png", scale=0.1)
-
+            self.obstacle.texture = arcade.load_texture(file_name="images/platform2.png", scale=0.1)
             self.obstacles_list.append(self.obstacle)
         
     def on_draw(self):
         arcade.start_render()
         self.sprite1.draw()
-
+    
         for obs in self.obstacles_list:
             obs.draw()
 
+        if self.sprite1.center_y < 20:
+            self.sprite1.center_y = 20
+            self.sprite1.change_y = 0
+            # Draw the game over menu if the player lost and the restart play button
+            texture = arcade.load_texture(file_name = "images/gameover.png")
+            arcade.draw_texture_rectangle(WIDTH//2, HEIGHT//2, 200, 100, texture, 0)
+       
     def update(self, delta_time):
+        global BOUNCINESS
+
         self.sprite1.update()
 
         self.sprite1.change_y -= GRAVITY 
 
         #Hit the bottom
-        if self.sprite1.center_y < 25 and self.sprite1.change_y < 0:
-
-            if self.sprite1.change_y * -1 > GRAVITY * 15:
-                self.sprite1.change_y *= -BOUNCINESS
-            else:
-                self.sprite1.change_y *= -BOUNCINESS / 2
-
 
         #if ball goes beyond width
-        elif self.sprite1.center_x > WIDTH + 25:
+        if self.sprite1.center_x > WIDTH + 25:
             self.sprite1.center_x = -25
 
         elif self.sprite1.center_x < -25:
@@ -92,8 +92,6 @@ class MyGame(arcade.Window):
     def on_key_release(self, key, key_modifiers):
         if key == arcade.key.D or key == arcade.key.A:
             self.sprite1.change_x = 0
-
-
 
 def main():
     game = MyGame(WIDTH, HEIGHT, 'test')
